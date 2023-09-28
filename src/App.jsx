00895c086fda22b76634b504d7608912528bb5a2
awakenjourney <mayer.sombat@gmail.com>
2023-09-28 17:53:09 -0600

    add media query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "../src/styles/Homepage.css";
 import "../src/styles/Navbar.css";
 import "../src/styles/Footer.css";
-import "../src/styles/Login.css";
 import "../src/styles/App.css";
 import "../src/styles/Form.css";
 import "bootstrap/dist/css/bootstrap.css";
-
+import "../src/styles/Media-query.css";
 import Header from "./assets/Header";
 import PagesContainer from "./components/PagesContainer";
 import Footer from "./components/Footer";
